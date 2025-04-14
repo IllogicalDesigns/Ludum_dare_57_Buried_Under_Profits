cc6407239314a52e03596cb3bc646518680851e1
CG <tinykidtoodotcom@yahoo.com>
2025-04-14 12:17:05 -0700

    Update damage indicators, Added pShrimp audio

--- ludum_dare_57/Assets/Scripts/Ai/PistolShrimps.cs
+++ ludum_dare_57/Assets/Scripts/Ai/PistolShrimps.cs
@@ -21,6 +21,12 @@ public class PistolShrimps : MonoBehaviour {
 
     bool isShooting;
 
+    [Space]
+    [SerializeField] AudioClip gunShotSFX;
+    [SerializeField] float preGunShot = 0.25f;
+    [SerializeField] AudioClip preGunShotSfx;
+    bool hasPreShotPlayed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         attackPoint = GameObject.Find("AttackPoint").transform;
@@ -44,6 +50,11 @@ public class PistolShrimps : MonoBehaviour {
 
         if (!isShooting) return;
 
+        if (timer <= preGunShot && !hasPreShotPlayed) {
+            AudioManager.instance.PlaySoundOnPoint(preGunShotSfx, transform.position);
+            hasPreShotPlayed = true;
+        }
+
         if (timer > 0) timer -= Time.deltaTime;
         else if (timer <= 0) {
             transform.LookAt(player.transform);
@@ -52,6 +63,9 @@ public class PistolShrimps : MonoBehaviour {
 
             var shot = Instantiate(launchedObject, launchPoint.transform.position, launchPoint.transform.rotation);
             shot.transform.LookAt(player.transform);
+
+            AudioManager.instance.PlaySoundOnPoint(gunShotSFX, transform.position);
+            hasPreShotPlayed = false;
         }
     }
 
@@ -61,6 +75,7 @@ public class PistolShrimps : MonoBehaviour {
         isShooting = true;
         gameObject.SendMessage(Threat.becomeThreatString);
         transform.LookAt(attackPoint);
+        hasPreShotPlayed = false;
     }
 
     public void OnHit(DamageInstance damageInstance) {
